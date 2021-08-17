a39e00ba99f984a5f6cd845adc28522b52df80b2
Jimmy Pham <jimmyp.dev3@gmail.com>
2021-08-17 09:06:03 -0700

    adding greeting file

---
+++ greetings.js
@@ -0,0 +1,28 @@
+/*
+Use the strings in the arrays to form a sentence like: "Hi, my name is <Name>
+Be sure to have beginning words of sentences with the proper case.
+
+Console log the result
+
+Use a different greeting word for each gender
+Use "his" for men, "her" for women, "their" for nonbinary.
+
+Use "my" if the isSelf argument is true
+
+const greetingWords = ["hello","hi","hey"];
+const prefixes = ["my", "his", "her", "their"];
+*/
+
+/**
+ @param {string} name
+        name of person
+ @parm  {string} gender
+        Possible values of "gender" argument are "male", "female", "nonbinary"
+ @param {boolean} isSelf
+ @returns {string}
+          greeting sentence
+*/
+function greeting(name, gender, isSelf) {
+    // do stuff here ...
+    return  ``; // use template literal for string to return
+  }
